a59c36c1113b58898879fc88f6decc2c1e0ced99
achuthaperumal <39347191+achuthaperumal@users.noreply.github.com>
2021-11-27 00:09:12 +0530

    Integrated Vision Library into TestApplication to convert raw data to image and display it in external windows.

--- BonTechWrapperLibrary/TestApplication/TestApplication.cpp
+++ BonTechWrapperLibrary/TestApplication/TestApplication.cpp
@@ -33,50 +33,68 @@ int main()
     int z = x ^ y;
     std::cout << z<<std::endl;*/
 
-    Image* myImage;
-    int handle, y, nErrorCode;
+    Image *calibratedImage, *uncalibratedImage;
+    int handle1, handle2, y, nErrorCode;
     unsigned int nHostIP, nSensorIP;
     
     nHostIP = 84125888;                                             //192.168.3.5
     nSensorIP = 4026771648;                                         //192.168.3.240
     
     unsigned short* pImage = new unsigned short[3072 * 3072];
-    /*unsigned short* pcalImage = new unsigned short[3072 * 3072];*/
     
-    LPCTSTR  ConfigDir = TEXT("D:\\BSDx64\\192.168.3.240\\Config");
-    LPCTSTR  calRefPath = TEXT("D:\\BSDx64\\192.168.3.240\\Reference");
-    /*CRect rectMargin(0, 0, 3072, 3072);*/
-        
+    LPCTSTR  ConfigDir = TEXT("C:\\BSD\\192.168.3.240\\Config");
+    LPCTSTR  calRefPath = TEXT("C:\\BSD\\192.168.3.240\\Reference");
+    /*CRect rectMargin(0, 0, 3072, 3072);*/   
+
     imaqSetWindowThreadPolicy(IMAQ_SEPARATE_THREAD);
-    imaqGetWindowHandle(&handle);
-    imaqSetWindowSize(handle, 1280, 720);
-    imaqSetWindowZoomToFit(handle, 1);
+    calibratedImage = imaqCreateImage(IMAQ_IMAGE_U16, 3);   
+    uncalibratedImage = imaqCreateImage(IMAQ_IMAGE_U16, 3);
 
-    myImage = imaqCreateImage(IMAQ_IMAGE_U16, 3);   
-
+    //Connect to Sensor
     nErrorCode = ImageCapConnectSensor(nHostIP, nSensorIP, ConfigDir);
     std::cout << hex << "Connect Status: "<<nErrorCode<<std::endl;
 
+    //Acquisition Mode Set
     nErrorCode = ImageCapModeChange(nSensorIP, IMAGECAP_SOFTTRIGGER_MODE);
     std::cout << hex << "Acquisition Mode Status: " << nErrorCode << std::endl;
+
+    cout << "Press any key to Acquire Calibrated Image: ";
+    cin >> y;
+
+    //Acquire Calibrated Image
+    imaqGetWindowHandle(&handle1);
+    imaqSetWindowTitle(handle1, "Calibrated Image");
+    imaqSetWindowSize(handle1, 1280, 720);
+    imaqSetWindowZoomToFit(handle1, 1);
 
     nErrorCode = ImageCapImageAcquistion(nSensorIP, pImage, SOFTTRIGGER_INSTANT_MODE,calRefPath);
     std::cout << hex << "Acquisition Status: " << nErrorCode << std::endl;
 
+    imaqArrayToImage(calibratedImage,pImage,3072,3072);
+    imaqDisplayImage(calibratedImage, handle1, 1);
+
+    //Acquire Uncalibrated Image
+    cout << "Press any key to Acquire Uncalibrated Image: ";
+    cin >> y;
+    imaqGetWindowHandle(&handle2);
+    imaqSetWindowTitle(handle2, "Uncalibrated Image");
+    imaqSetWindowSize(handle2, 1280, 720);
+    imaqSetWindowZoomToFit(handle2, 1);
+    
+    nErrorCode = ImageCapImageAcquistion(nSensorIP, pImage, SOFTTRIGGER_INSTANT_MODE, NULL);
+    std::cout << hex << "Acquisition Status: " << nErrorCode << std::endl;
+
+    imaqArrayToImage(uncalibratedImage, pImage, 3072, 3072);
+    imaqDisplayImage(uncalibratedImage, handle2, 1);
+
+    cout << "Press any key to Close the Application! ";
+    cin >> y;
+
     /*nErrorCode = ImageCalibration(pImage, calRefPath, 3072, 3072, IMAGECAP_TRIGGER_MODE, rectMargin, FALSE);
     std::cout << dec <<"Calibration Status: " << nErrorCode << std::endl;*/
 
-    imaqArrayToImage(myImage,pImage,3072,3072);
-
-    /*for (y = 0; y < LVHeight; ++y)
-    {
-        memcpy((unsigned short*)myImageInfo.imageStart + myImageInfo.pixelsPerLine * y, pImage + LVWidth * y, LVWidth*2);
-    }*/
-
-    imaqDisplayImage(myImage, handle, 1);
-    cin >> y;
-
-    imaqDispose(myImage);
+    imaqDispose(calibratedImage);
+    imaqDispose(uncalibratedImage);
     delete[] pImage;
     nErrorCode = ImageCapDisConnectSensor(nSensorIP);
 }
